b848655da0ee56aaa2b98226ca29c27085b91cd0
swatantra gupta <swatantra gupta>
2023-10-10 22:02:06 +0530

    day 1 tasks completed

---
+++ Operators.js
@@ -0,0 +1,16 @@
+// operators are : 
+/*
++, -, *, /, %, ==, ===, ++, --, <=, >= and much more
+ */
+
+let a = 12;
+let b = 23;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a<=b);
+console.log(a>=b);
+console.log(a==b);
+console.log(a===b);
---
+++ TemplateLiterals.js
@@ -0,0 +1,12 @@
+/*
+Template literals is just a way or method to write something within
+the quotes of ``.
+This also called as the string interpolation
+*/
+
+let myName = "swatantra gupta";
+let myAge = 23
+let location = "mathura, U.P."
+
+let intro = `Hey there, my name is ${myName}, my age is ${myAge} and i live in ${location}`;
+console.log(intro);
